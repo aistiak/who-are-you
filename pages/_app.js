9cbe8b71dc17ac5e24ebedf080de1761b2124493
aistiak <aristiak1994@gmail.com>
2021-09-10 10:50:36 +0600

    added GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Script from "next/script";
+
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-BK2EHYVS1S"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-BK2EHYVS1S');
+// </script>
 
 const client = new ApolloClient({
   uri: "https://rickandmortyapi.com/graphql",
@@ -12,9 +19,24 @@ const client = new ApolloClient({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />{" "}
-    </ApolloProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-BK2EHYVS1S"}
+      />
+      <Script strategy="lazyOnload" id={"ga-script"}>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-BK2EHYVS1S');
+        `}
+      </Script>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />{" "}
+      </ApolloProvider>
+    </>
   );
 }
 
