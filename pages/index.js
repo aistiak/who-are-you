edccf05b7854419521a3f9ccceb8dd682330db25
aistiak <aristiak1994@gmail.com>
2021-09-10 09:55:28 +0600

    user next/image

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { GET_CHARECTER } from "../graphql/queries";
 import styles from "../styles/Home.module.css";
-
+import Image from "next/image";
 const randomId = (_) => parseInt(Math.random() * 671);
 
 export default function Home() {
-
   const [characterId, setCharacterId] = useState(randomId());
   const { loading, error, data } = useQuery(GET_CHARECTER, {
     variables: {
@@ -20,14 +19,20 @@ export default function Home() {
       setCharacter(data.character);
     }
   }, [data]);
-  if(loading) return  <div className={styles.container}> loading...</div> ;
+  if (loading) return <div className={styles.container}> loading...</div>;
   return (
     <div className={styles.container}>
       You Are
       {character && (
         <>
           <h2> {character.name} </h2>
-          <img src={character.image} alt={character.name} />
+          <Image
+            loader={() => character.image}
+            src="vercel.svg"
+            alt={character.name}
+            height={350}
+            width={350}
+          />
         </>
       )}
       <button
